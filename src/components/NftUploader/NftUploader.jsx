b2d8038167243b610d8618131468879af7384f97
Koshi-tama <1879tk@gmail.com>
2023-01-08 08:44:18 +0900

    upload to github

--- src/components/NftUploader/NftUploader.jsx
+++ src/components/NftUploader/NftUploader.jsx
@@ -1,11 +1,120 @@
 import { Button } from "@mui/material";
 import React from "react";
+import { useState, useEffect } from "react";
 import ImageLogo from "./image.svg";
 import "./NftUploader.css";
+import Web3Mint from "../../utils/Web3Mint.json";
+
+import { ethers } from "ethers";
+import { Web3Storage } from "web3.storage";
 
 const NftUploader = () => {
+  const [currentAccount, setCurrentAccount] = useState("");
+  console.log("currentAccount:", currentAccount);
+
+  const checkIfWalletIsConnected = async () => {
+    const { ethereum } = window;
+    if (!ethereum) {
+      console.log("Make sure you have MetaMask!");
+      return;
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      setCurrentAccount(account);
+    } else {
+      console.log("No Authorize account found");
+    }
+  };
+
+  const imageToNFT = async (e) => {
+    const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDQzZGNiQjdjRTA1MjEzQkY0YWE3ZjI3M2Q0NUFCNTU3YThlQ2Q1YUEiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NzMxMzEwNjAwMTIsIm5hbWUiOiJ3ZWIzTkZUTWludCJ9.poLyrkOpyubCpca-PG8hOHacuCpkPfHTy7NtOrbEBfY";
+    const client = new Web3Storage({ token: API_KEY });
+    const image = e.target;
+    console.log(image);
+
+    const rootCid = await client.put(image.files, {
+      name: "experiment",
+      maxRetries: 3,
+    });
+    const res = await client.get(rootCid);
+    const files = await res.files();
+    for (const file of files) {
+      console.log("file.cid:", file.cid);
+      askContractToMintNft(file.cid);
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        alert("Get Metamask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      console.log("Connected", accounts[0]);
+
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const askContractToMintNft = async (ipfs) => {
+    const CONTRACT_ADDRESS = "0x0E8CF40880EE1730c0E4C283998D1209b8eB0A14";
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          Web3Mint.abi,
+          signer
+        );
+        console.log("Going to pop wallet now to pay gas...");
+        let nftTxn = await connectedContract.mintIpfsNFT("sample", ipfs);
+        console.log("Minting... wait!");
+        await nftTxn.wait();
+        console.log(
+          `Minted, see transaction: https://goerli.etherscan.io/tx/${nftTxn.hash}`
+        );
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const renderNotCOnnectedContainer = () => (
+    <button
+      onClick={connectWallet}
+      className="cta-button connect-wallet-button"
+    >
+      Connect to Wallet
+    </button>
+  );
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
   return (
     <div className="outerBox">
+      {currentAccount === "" ? (
+        renderNotCOnnectedContainer()
+      ) : (
+        <p>If you choose image, you can mint your NFT</p>
+      )}
       <div className="title">
         <h2>NFTアップローダー</h2>
         <p>JpegかPngの画像ファイル</p>
@@ -15,12 +124,24 @@ const NftUploader = () => {
           <img src={ImageLogo} alt="imagelogo" />
           <p>ここにドラッグ＆ドロップしてね</p>
         </div>
-        <input className="nftUploadInput" multiple name="imageURL" type="file" accept=".jpg , .jpeg , .png"  />
+        <input
+          className="nftUploadInput"
+          multiple
+          name="imageURL"
+          type="file"
+          accept=".jpg , .jpeg , .png"
+          onChange={imageToNFT}
+        />
       </div>
       <p>または</p>
       <Button variant="contained">
         ファイルを選択
-        <input className="nftUploadInput" type="file" accept=".jpg , .jpeg , .png"/>
+        <input
+          className="nftUploadInput"
+          type="file"
+          accept=".jpg , .jpeg , .png"
+          onChange={imageToNFT}
+        />
       </Button>
     </div>
   );
